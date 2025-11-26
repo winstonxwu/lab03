4a763899dae11e91d1439ab6dd4a4ea6f41cef7a
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-11-25 19:55:29 -0500

    bug fix

--- pages/2_Gemini_Processing.py
+++ pages/2_Gemini_Processing.py
@@ -17,24 +17,27 @@ st.write(
 nba = players.get_players()
 nba_names = [p["full_name"] for p in nba]
 tab1, tab2 = st.tabs(["Select Players", "View Analysis"])
+
 with tab1:
+    request_data = False
     st.subheader("Choose two nba players to compare")
 
     p1 = st.selectbox("Player 1", nba_names, index=0)
     p2 = st.selectbox("Player 2", nba_names, index=1)
 
 
+    st.caption("Select a focus for the comparison")
+    focus = st.selectbox("Main focus",["Completeness", "Scoring", "Playmaking", "Defense", "Rebounding"])
+
+
     submit = st.button("Submit")
 
     if submit:
+        request_data = True
         if p1 == p2:
             st.warning("Please use two different players to compare")
         p1_id = [p for p in nba if p['full_name'] == p1][0]['id']
         p2_id = [p for p in nba if p['full_name'] == p2][0]['id']
-
-
-        st.caption("Select a focus for the comparison")
-        focus = st.selectbox("Main focus",["Completeness", "Scoring", "Playmaking", "Defense", "Rebounding"])
 
         p1_stats = playercareerstats.PlayerCareerStats(player_id=p1_id)
         df_p1 = p1_stats.get_data_frames()[0]
@@ -44,9 +47,10 @@ with tab1:
 
 
 with tab2:
-    if submit:
+    if request_data:
         response = model.generate_content(f"Compare two NBA players based on their stats from their career and our main focus {focus}. The two players we want to compare are {p1} and {p2}. The career stats for {p1} are {df_p1} and the career stats for {p2} are {df_p2}. Try your best to be unbiased and try to adjust for different eras of play and pace if two players are from different times.")
         st.subheader("The analysis:")
         st.write(response.text)
+        
     else:
         st.write("Go back and pick players and focus!")
