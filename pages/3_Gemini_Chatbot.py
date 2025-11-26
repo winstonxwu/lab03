4242e71b30cba20b6860e73861942b142e9280ac
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-11-25 20:57:13 -0500

    corrected version

--- pages/3_Gemini_Chatbot.py
+++ pages/3_Gemini_Chatbot.py
@@ -21,8 +21,8 @@ if chatPrompt:
     try:
         user.write(chatPrompt)
         ai = st.chat_message("ai")
-        response = model.generate_content(f"You are a specialized nba expert on the 2025-2026 season. Respond to the prompt {chatPrompt} briefly, with a maximum response of 150-200 words. Past conversations: {st.session_state.messages.join(",")}. Use information from the 2025-2026 season: {st.session_state.nba_data}")
+        response = model.generate_content(f"You are a specialized nba expert on the 2025-2026 season. Respond to the prompt {chatPrompt} briefly, with a maximum response of 150-200 words. Past conversations: {", ".join(st.session_state.messages)}. Use information from the 2025-2026 season: {st.session_state.nba_data}")
         ai.write(response.text)
-        st.session_state.messages.append(chatPrompt+response)
+        st.session_state.messages.append(chatPrompt, response.text)
     except:
         ai.write("The Gemini API has errored. This is likely due to a rate error, so please wait for requests to reload.")
