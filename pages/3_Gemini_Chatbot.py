f7ccfcfcfd0067fdeee2a86df74992d4cd9b50de
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-11-25 20:21:32 -0500

    chat separation

--- pages/3_Gemini_Chatbot.py
+++ pages/3_Gemini_Chatbot.py
@@ -9,7 +9,8 @@ with st.chat_message("user"):
     if chatPrompt:
         try:
             st.write(chatPrompt)
-            response = model.generate_content(f"You are a specialized nba expert. Respond to the prompt {chatPrompt} briefly, in about 150-200 words.")
-            st.write(response.text)
+            ai = st.chat_message("ai")
+            response = model.generate_content(f"You are a specialized nba expert. Respond to the prompt {chatPrompt} briefly, with a maximum response of 150-200 words.")
+            ai.write(response.text)
         except:
-            st.write("The Gemini API has errored. This is likely due to a rate error. Please wait for requests to reload.")
+            ai.write("The Gemini API has errored. This is likely due to a rate error. Please wait for requests to reload.")
